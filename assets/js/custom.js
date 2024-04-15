c05ce434872078bf38476184192ae7530429c148
sallyselva <166712783+sallyselva@users.noreply.github.com>
2024-04-15 15:38:13 +0530

    Update custom.js

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -9,3 +9,11 @@
  * 
  * To register a constrol simmessages, use addSimMessageHandler
  */
+addSimMessageHandler("web", (data) => {
+    switch(data.action) {
+        case "open":
+            const url = data.url;
+            window.open(url, "_blank");
+            break;
+    }                    
+}) 
